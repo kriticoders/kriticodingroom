df3d2c35aeb2c4638a93ef08985f9e238246507c
kriticoders <kritikumari68394@gmail.com>
2024-12-07 12:43:14 +0530

    function

--- C Program/first.c
+++ C Program/first.c
@@ -1,74 +1,19 @@
-# include<math.h>
-# include<math.h>
-# include<time.h>
+// program to find sum of two numbers//
+#include <stdio.h>
+int sum(int x, int y); // function declration
 
 int main()
 {
-  clock_t time_start,time_end;
-  time_start = clock();
-  int flag =0;
-  int i = 0;
-  int num = 0;
-  printf("Enter a number \t");
-  scanf("%d",&num);
-
-  for(i=2;i<num;i++)
-  {
-
-
-  if(num%i==0)
-  {
-    flag=1;
-    break;
-  }
-  }
-     if(flag==0)
-     {
-      printf("Its a prime number\n");
-     }
-     else
-     {
-      printf("Its not a prime number\n");
-     }
-      
-      time_t end=clock();
-      printf("Exection time %f \n",(float)('time_end-time_start')/CLOCKS_PER_SEC);
-
-  
-
-
+    int a, b, s;
+    printf("enter the values of a and b number is :");
+    scanf("%d %d", &a, &b);
+    s = sum(a, b); // function call
+    printf("sum of %d and %d is %d\n", a, b, s);
 }
-
-
-
-
-
-
-    
-    
-
-
-
-
-
-
- 
-
-
-
-
-
-
-
- 
- 
-
-
-
- 
-
-
-
-  
-
-
+int sum(int x, int y)
+{
+    // function definition
+    int s;
+    s = x + y;
+    return s;
+}
